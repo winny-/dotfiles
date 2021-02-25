10fe55cd9bcc7401de0f2f3dd67943635c7ac4a4
Winston Weinert <winston@ml1.net>
2021-02-24 20:39:37 -0600

    disable LS_COLORS for now

--- modules/bash/image/.bashrc.d/21-colors.bash
+++ modules/bash/image/.bashrc.d/21-colors.bash
@@ -1,5 +1,4 @@
 # Originally grabbed from GRML's zshrc.
-
 export LESS_TERMCAP_mb=$'\E[01;31m'
 export LESS_TERMCAP_md=$'\E[01;31m'
 export LESS_TERMCAP_me=$'\E[0m'
@@ -7,3 +6,6 @@ export LESS_TERMCAP_se=$'\E[0m'
 export LESS_TERMCAP_so=$'\E[01;44;33m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[01;32m'
+
+# Disable extra colors in ls for now
+export LS_COLORS=
