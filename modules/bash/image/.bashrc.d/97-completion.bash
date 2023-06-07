4474706e6c8fa42c01cec9a1f7a0fbda46079a72
Winston Weinert <git@winny.tech>
2023-06-07 16:37:38 -0500

    add: flyctl bash completion

--- modules/bash/image/.bashrc.d/97-completion.bash
+++ modules/bash/image/.bashrc.d/97-completion.bash
@@ -1,4 +1,4 @@
-for cmd in jhmod vultr-cli; do
+for cmd in jhmod vultr-cli flyctl; do
     if command -v "$cmd" >/dev/null; then
         # shellcheck disable=SC1090
         source <("$cmd" completion bash)
