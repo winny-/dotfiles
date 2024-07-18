1e3a0c45b8177b3ee60585fddaa118ea6f4d3e7d
Winston Weinert <git@winny.tech>
2024-07-17 20:23:20 -0500

    fix: shush perl init again

--- modules/bash/image/.bashrc.d/20-perl.bash
+++ modules/bash/image/.bashrc.d/20-perl.bash
@@ -1,5 +1,5 @@
 perldir="$HOME/perl5/lib/perl5"
-if command -v perl && [[ -d $perldir ]]; then
-    eval "$(perl -I"$perldir" -Mlocal::lib)" > /dev/null
+if command -v perl &>/dev/null && [[ -d $perldir ]]; then
+    eval "$(perl -I"$perldir" -Mlocal::lib)"
 fi
 unset perldir
