febad7642c6261c76c751e775c48ca33bc1dff0d
Winston Weinert <git@winny.tech>
2024-07-17 20:21:10 -0500

    fix: shush perl5 initialization script

--- modules/bash/image/.bashrc.d/20-perl.bash
+++ modules/bash/image/.bashrc.d/20-perl.bash
@@ -1,5 +1,5 @@
 perldir="$HOME/perl5/lib/perl5"
 if command -v perl && [[ -d $perldir ]]; then
-    eval "$(perl -I"$perldir" -Mlocal::lib)"
+    eval "$(perl -I"$perldir" -Mlocal::lib)" > /dev/null
 fi
 unset perldir
